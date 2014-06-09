0fceca33c72aad402ef59f49e17e16f15d22c3f1
Ankit Modi <a.modi1422@gmail.com>
2014-06-09 17:31:47 +0530

    Create Node.java

---
+++ DepthFirstTraversal/Node.java
@@ -0,0 +1,42 @@
+class Node{
+	private Object data;
+	private Node leftchild;
+	private Node rightchild;
+	
+	public Node(){
+		
+		this.leftchild = null;
+		this.rightchild = null;
+	}
+	
+	public Node(Object elem){
+		this.data = elem;
+		this.leftchild = null;
+		this.rightchild = null;
+	}
+	
+	public Object getData(){
+		return data;
+	}
+	
+	public void setData(Object inputData){
+		data = inputData;
+	}
+	
+	public Node getLeftchild(){
+		return leftchild;
+	}
+	
+	public void setLeftchild(Node inputNode){
+		leftchild = inputNode;
+	}
+	
+	public Node getRightchild(){
+		return rightchild;
+	}
+	
+	public void setRightchild(Node inputNode){
+		rightchild = inputNode;
+	}
+	
+}
