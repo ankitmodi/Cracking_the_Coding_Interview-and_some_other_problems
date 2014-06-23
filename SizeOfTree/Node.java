f91f3f6c0330cb6376727d405815200f341a3ec2
Ankit Modi <a.modi1422@gmail.com>
2014-06-23 16:46:40 +0530

    Create Node.java

---
+++ SizeOfTree/Node.java
@@ -0,0 +1,84 @@
+class Node
+{
+	private Object data;
+	private Node leftchild;
+	private Node rightchild;
+	private Node next;
+
+	
+	
+	public Node()
+	{
+
+		this.leftchild = null;
+		this.rightchild = null;
+		this.next = null;
+	}
+
+	
+	
+	public Node(Object elem)
+	{
+		this.data = elem;
+		this.leftchild = null;
+		this.rightchild = null;
+		this.next = null;
+	}
+
+	
+	
+	public Object getData()
+	{
+		return data;
+	}
+
+	
+	
+	public void setData(Object inputData)
+	{
+		data = inputData;
+	}
+
+	
+	
+	public Node getLeftchild()
+	{
+		return leftchild;
+	}
+
+	
+	
+	public void setLeftchild(Node inputNode)
+	{
+		leftchild = inputNode;
+	}
+
+	
+	
+	public Node getRightchild()
+	{
+		return rightchild;
+	}
+
+	
+	
+	public void setRightchild(Node inputNode)
+	{
+		rightchild = inputNode;
+	}
+
+	
+	
+	public Node getNext()
+	{
+		return next;
+	}
+
+	
+	
+	public void setNext(Node inputNode)
+	{
+		next = inputNode;
+	}
+
+}
