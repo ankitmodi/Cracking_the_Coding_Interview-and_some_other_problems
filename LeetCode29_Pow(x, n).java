203b00cc389d921c6b4e7c2572155993c31116ba
Ankit Modi <a.modi1422@gmail.com>
2014-09-03 13:55:34 +0530

    Create LeetCode29_Pow(x, n).java

---
+++ LeetCode29_Pow(x, n).java
@@ -0,0 +1,28 @@
+/*Implement pow(x, n). */
+
+public class Solution 
+{
+    public double pow(double x, int n) 
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        double temp = pow(x,n/2);
+        if(n%2==0)
+        {
+            return temp*temp;
+        }
+        else
+        {
+            if(n>0)
+            {
+                return temp*temp*x;
+            }
+            else
+            {
+                return (temp*temp)/x;
+            }
+        }
+    }
+}
