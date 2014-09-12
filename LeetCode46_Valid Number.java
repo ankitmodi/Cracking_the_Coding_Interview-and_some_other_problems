a5d9ab5113b94a292d0ac40dc8d8933f87790f60
Ankit Modi <a.modi1422@gmail.com>
2014-09-13 01:28:32 +0530

    Create LeetCode46_Valid Number.java

---
+++ LeetCode46_Valid Number.java
@@ -0,0 +1,25 @@
+/*Validate if a given string is numeric.
+
+Some examples:
+"0" => true
+" 0.1 " => true
+"abc" => false
+"1 a" => false
+"2e10" => true
+
+Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up front before implementing one. */
+
+
+public class Solution 
+{
+    public boolean isNumber(String s) 
+    {
+        s = s.trim();
+        if(s.isEmpty())
+        {
+            return false;
+        }
+        String pattern = "\\s*[+-]?((\\.\\d+)|(\\d+(\\.\\d*)?))(e[+-]?\\d+)?\\s*";
+        return s.matches(pattern);
+    }
+}
