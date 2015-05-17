4d459ab34599c8adbeb0a786a0c64ca214d30f00
Ankit Modi <a.modi1422@gmail.com>
2015-05-17 12:39:25 +0530

    Update 3_PASCAL2.java

--- InterviewBit/3_PASCAL2.java
+++ InterviewBit/3_PASCAL2.java
@@ -26,19 +26,16 @@ public class Solution {
 	    
 	    for(int i=2; i<=A; i++)
 	    {
-	        ArrayList<Integer> tmp = list2;
-	        ArrayList<Integer> new_tmp = new ArrayList<Integer>();
-	        new_tmp.add(1);
-	        int size = tmp.size();
+	        list1 = list2;
+	        list2 = new ArrayList<Integer>();
+	        list2.add(1);
+	        int size = list1.size();
 	        
 	        for(int j = 1; j<size; j++)
 	        {
-	            new_tmp.add(tmp.get(j) + tmp.get(j-1));
+	            list2.add(list1.get(j) + list1.get(j-1));
 	        }
-	        new_tmp.add(1);
-	        
-	        
-	        list2 = new_tmp;
+	        list2.add(1);
 	        
 	    }
 	    
